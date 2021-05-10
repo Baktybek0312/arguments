a660093ac30929dd9ef73da3775c5814f4b0bbc4
Baha Bro <toktobekov1995@gmail.com>
2021-05-10 20:40:20 +0600

    исправлено ошибка arg3.py

--- arg3.py
+++ arg3.py
@@ -1,4 +1,13 @@
 # Напишите функцию которая принимает 2 Dictionary и добавляет втрорую Dictionary к первой.
 
-# def Dictionary(a, b):
-#     a =
+def dictionary(**kwargs):
+    my_dict = [ ]
+    my_dict.append([kwargs["key"]])
+    my_dict.append([kwargs["key2"]])
+    return my_dict
+
+
+a = {'a': 1, 'b': 2}
+b = {'c': 3, 'd': 4}
+
+print(dictionary(key=a, key2=b))
